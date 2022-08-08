ef8fab733a0593ab1f6095f97d8d736ad4ed7d68
Muhamadrafliseptian <rafliseptiannn25@gmail.com>
2022-08-09 00:30:29 +0700

    update login

--- resources/views/layouts/partials/menu/study-case.blade.php
+++ resources/views/layouts/partials/menu/study-case.blade.php
@@ -22,9 +22,7 @@
                     <div class="row row-cols-1 row-cols-md-2 g-4">
                         <div class="col">
                             <div class="card2 me-2 mb-2" style="border: none; box-shadow: 0px 3px 3px rgb(174, 214, 255)">
-                                <iframe autoplay="1" controls="0" class="w-100" style="height: 385px"
-                                    src="{{ url('https://www.youtube.com/embed/tgbNymZ7vqY?autoplay=1" allow="autoplay') }}">
-                                </iframe>
+                                <img class="w-100 img-fluid" height="385px" src="{{ url('assets/img/1.jpg') }}" alt="">
                                 <div class="card-body">
                                     <img src="{{ asset('assets/img/clients/client-1.png') }}" width="30px" height="30px"
                                         class="logo" alt="" />
@@ -42,9 +40,7 @@
                         </div>
                         <div class="col">
                             <div class="card2" style="border: none; box-shadow: 0px 3px 3px rgb(174, 214, 255)">
-                                <iframe autoplay="1" controls="0" class="w-100" style="height: 385px"
-                                    src="https://www.youtube.com/embed/c9Q4XHIQHcA">
-                                </iframe>
+                           <img class="w-100 img-fluid" height="385px" src="{{ url('assets/img/1.jpg') }}" alt="">
                                 <div class="card-body">
                                     <img src="{{ asset('assets/img/clients/client-2.png') }}" width="30px" height="30px"
                                         class="logo" alt="" />
@@ -62,9 +58,7 @@
                         </div>
                         <div class="col">
                             <div class="card2 me-2 mb-2" style="border: none; box-shadow: 0px 3px 3px rgb(174, 214, 255)">
-                                <iframe autoplay="10" controls="0" class="w-100" style="height: 385px"
-                                    src="https://www.youtube.com/embed/tgbNymZ7vqY">
-                                </iframe>
+                                <img class="w-100 img-fluid" height="385px" src="{{ url('assets/img/1.jpg') }}" alt="">
                                 <div class="card-body">
                                     <img src="https://upload.wikimedia.org/wikipedia/commons/f/f9/Phoenicopterus_ruber_in_S%C3%A3o_Paulo_Zoo.jpg"
                                         width="30px" height="30px" class="logo" alt="" />
@@ -81,9 +75,7 @@
                         </div>
                         <div class="col">
                             <div class="card2" style="border: none; box-shadow: 0px 3px 3px rgb(174, 214, 255)">
-                                <iframe autoplay="10" controls="0" class="w-100" style="height: 385px"
-                                    src="https://www.youtube.com/embed/vg0XKHgb9qQ">
-                                </iframe>
+                            <img class="w-100 img-fluid" height="385px" src="{{ url('assets/img/1.jpg') }}" alt="">
                                 <div class="card-body">
                                     <img src="https://upload.wikimedia.org/wikipedia/commons/f/f9/Phoenicopterus_ruber_in_S%C3%A3o_Paulo_Zoo.jpg"
                                         width="30px" height="30px" class="logo" alt="" />
