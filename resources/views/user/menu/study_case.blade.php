56b7b4b634ba7844d7ff30341bb0d455f4518bba
Gunawan19621 <gunawan19621@gmail.com>
2022-08-24 12:20:11 +0700

    study case

--- resources/views/user/menu/study_case.blade.php
+++ resources/views/user/menu/study_case.blade.php
@@ -5,98 +5,130 @@
             <h1 class="text-center text-light">Study Case</h1>
         </div>
     </section>
-    <br>
-    <br>
-
-    <section id="featured-services" class="featured-services">
-        <div class="container" data-aos="fade-up">
-            <div class="section-title">
+    <section id="study-case" class="study-case">
+      <div class="container" data-aos="fade-up">
+         <div class="section-title mt-0">
                 <h2>Study Case</h2>
-                <h3><span>Check Our Customer Study Case</span></h3>
+                <p>IDM can increase efficiency from the start, enabling customers to enjoy the full benefits of integrated
+                    and enterprise solutions in a more affordable way.</p>
             </div>
+        <div class="row g-5">
 
-            <div class="container mt-5">
-                <div class="row row-cols-1 row-cols-md-2 g-4">
-                    <div class="col">
-                        <div class="card2 me-2 mb-2" style="border: none; box-shadow: 0px 3px 3px rgb(174, 214, 255)">
-                            <img  class="w-100" style="height: 385px"
-                                src="{{ url('assets/img/1.jpg') }}">
-                            <div class="card-body">
-                                <img src="{{ asset('assets/img/clients/client-1.png') }}" width="30px" height="30px"
-                                    class="logo" alt="" />
-                                <h3 class="card-title">MYOB</h3>
-                                <p class="card-text">
-                                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam
-                                    aliquid dolor, laudantium vero quae omnis vel itaque. Quidem
-                                    assumenda dolore praesentium dolor exercitationem, laborum ullam
-                                    deleniti delectus eos eum facilis?
-                                </p>
-                                <a href="{{ url('single_partner') }}" class="btn btn-primary btn-sm text-md-end">Read
-                                    More</a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col">
-                        <div class="card2" style="border: none; box-shadow: 0px 3px 3px rgb(174, 214, 255)">
-                            <img  class="w-100" style="height: 385px"
-                                src="{{ url('assets/img/1.jpg') }}">
-                            <div class="card-body">
-                                <img src="{{ asset('assets/img/clients/client-2.png') }}" width="30px" height="30px"
-                                    class="logo" alt="" />
-                                <h3 class="card-title">OSLOG Mobile</h3>
-                                <p class="card-text">
-                                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam
-                                    aliquid dolor, laudantium vero quae omnis vel itaque. Quidem
-                                    assumenda dolore praesentium dolor exercitationem, laborum ullam
-                                    deleniti delectus eos eum facilis?
-                                </p>
-                                <a href="{{ url('single_partner') }}" class="btn btn-primary btn-sm text-md-end">Read
-                                    More</a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col">
-                        <div class="card2 me-2 mb-2" style="border: none; box-shadow: 0px 3px 3px rgb(174, 214, 255)">
-                            <iframe autoplay="10" controls="0" class="w-100" style="height: 385px"
-                                src="https://www.youtube.com/embed/tgbNymZ7vqY">
-                            </iframe>
-                            <div class="card-body">
-                                <img src="https://upload.wikimedia.org/wikipedia/commons/f/f9/Phoenicopterus_ruber_in_S%C3%A3o_Paulo_Zoo.jpg"
-                                    width="30px" height="30px" class="logo" alt="" />
-                                <h3 class="card-title">OSLOG Mobile</h3>
-                                <p class="card-text">
-                                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam
-                                    aliquid dolor, laudantium vero quae omnis vel itaque. Quidem
-                                    assumenda dolore praesentium dolor exercitationem, laborum ullam
-                                    deleniti delectus eos eum facilis?
-                                </p>
-                                <a href="" class="btn btn-primary btn-sm text-md-end">Read More</a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col">
-                        <div class="card2" style="border: none; box-shadow: 0px 3px 3px rgb(174, 214, 255)">
-                            <iframe autoplay="10" controls="0" class="w-100" style="height: 385px"
-                                src="https://www.youtube.com/embed/vg0XKHgb9qQ">
-                            </iframe>
-                            <div class="card-body">
-                                <img src="https://upload.wikimedia.org/wikipedia/commons/f/f9/Phoenicopterus_ruber_in_S%C3%A3o_Paulo_Zoo.jpg"
-                                    width="30px" height="30px" class="logo" alt="" />
-                                <h3 class="card-title">OSLOG Mobile</h3>
-                                <p class="card-text">
-                                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam
-                                    aliquid dolor, laudantium vero quae omnis vel itaque. Quidem
-                                    assumenda dolore praesentium dolor exercitationem, laborum ullam
-                                    deleniti delectus eos eum facilis?
-                                </p>
-                                <a href="" class="btn btn-primary btn-sm text-md-end">Read More</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+          <div class="col-lg-12">
+
+            <div class="row gy-4 posts-list">
+
+              <div class="col-lg-12">
+                <article class="d-flex flex-column">
+
+                  <div class="post-img">
+                    <img src="assets/img/blog/blog-1.jpg" alt=" " class="img-fluid w-100" data="zoom-in">
+                  </div>
+
+                  <h2 class="title">
+                    <a href="blog-details.html">Dolorum optio tempore voluptas dignissimos cumque fuga qui quibusdam quia</a>
+                  </h2>
+
+                  <div class="meta-top">
+                    <ul>
+                      <li class="d-flex align-items-center"><i class="bi bi-person"></i> <a href="blog-details.html">John Doe</a></li>
+                      <li class="d-flex align-items-center"><i class="bi bi-clock"></i> <a href="blog-details.html"><time datetime="2022-01-01">Jan 1, 2022</time></a></li>
+                    </ul>
+                  </div>
+
+                  <div class="content">
+                    <p>
+                      Similique neque nam consequuntur ad non maxime aliquam quas. Quibusdam animi praesentium. Aliquam et laboriosam eius aut nostrum quidem aliquid dicta.
+                    </p>
+                  </div>
+
+                  <div class="read-more mt-auto align-self-end">
+                    <a href="blog-details.html">Read More</a>
+                  </div>
+
+                </article>
+              </div><!-- End post list item -->
+
+              <div class="col-md-4">
+                <article class="d-flex flex-column">
+
+                  <div class="post-img">
+                    <img src="assets/img/blog/blog-3.jpg" alt="" class="img-fluid">
+                  </div>
+
+                  <h2 class="title">
+                    <a href="blog-details.html">Possimus soluta ut id suscipit ea ut. In quo quia et soluta libero sit sint.</a>
+                  </h2>
+
+                  <div class="meta-top">
+                    <ul>
+                      <li class="d-flex align-items-center"><i class="bi bi-person"></i> <a href="blog-details.html">John Doe</a></li>
+                      <li class="d-flex align-items-center"><i class="bi bi-clock"></i> <a href="blog-details.html"><time datetime="2022-01-01">Jan 1, 2022</time></a></li>
+                    </ul>
+                  </div>
+                  <div class="content">
+                    <p>
+                      Aut iste neque ut illum qui perspiciatis similique recusandae non. Fugit autem dolorem labore omnis et. Eum temporibus fugiat voluptate enim tenetur sunt omnis.
+                    </p>
+                  </div>
+                  <div class="read-more mt-auto align-self-end">
+                    <a href="blog-details.html">Read More</a>
+                  </div>
+                </article>
+              </div><!-- End post list item -->
+              <div class="col-md-4">
+                <article class="d-flex flex-column">
+                  <div class="post-img">
+                    <img src="assets/img/blog/blog-3.jpg" alt="" class="img-fluid">
+                  </div>
+                  <h2 class="title">
+                    <a href="blog-details.html">Distinctio provident quibusdam numquam aperiam aut</a>
+                  </h2>
+                  <div class="meta-top">
+                    <ul>
+                      <li class="d-flex align-items-center"><i class="bi bi-person"></i> <a href="blog-details.html">John Doe</a></li>
+                      <li class="d-flex align-items-center"><i class="bi bi-clock"></i> <a href="blog-details.html"><time datetime="2022-01-01">Jan 1, 2022</time></a></li>
+                    </ul>
+                  </div>
+                  <div class="content">
+                    <p>
+                      Expedita et temporibus eligendi enim molestiae est architecto praesentium dolores. Illo laboriosam officiis quis. Labore officia quia sit voluptatem nisi est dignissimos totam. Et voluptate et consectetur voluptatem id dolor magni impedit. Omnis dolores sit.
+                    </p>
+                  </div>
+                  <div class="read-more mt-auto align-self-end">
+                    <a href="blog-details.html">Read More</a>
+                  </div>
+                </article>
+              </div>
+              <div class="col-md-4">
+                <article class="d-flex flex-column">
+                  <div class="post-img">
+                    <img src="assets/img/blog/blog-3.jpg" alt="" class="img-fluid">
+                  </div>
+                  <h2 class="title">
+                    <a href="blog-details.html">Distinctio provident quibusdam numquam aperiam aut</a>
+                  </h2>
+                  <div class="meta-top">
+                    <ul>
+                      <li class="d-flex align-items-center"><i class="bi bi-person"></i> <a href="blog-details.html">John Doe</a></li>
+                      <li class="d-flex align-items-center"><i class="bi bi-clock"></i> <a href="blog-details.html"><time datetime="2022-01-01">Jan 1, 2022</time></a></li>
+                    </ul>
+                  </div>
+                  <div class="content">
+                    <p>
+                      Expedita et temporibus eligendi enim molestiae est architecto praesentium dolores. Illo laboriosam officiis quis. Labore officia quia sit voluptatem nisi est dignissimos totam. Et voluptate et consectetur voluptatem id dolor magni impedit. Omnis dolores sit.
+                    </p>
+                  </div>
+                  <div class="read-more mt-auto align-self-end">
+                    <a href="blog-details.html">Read More</a>
+                  </div>
+                </article>
+              </div>
             </div>
+          </div>
+
         </div>
 
-        </div>
+      </div>
     </section>
 @endsection
