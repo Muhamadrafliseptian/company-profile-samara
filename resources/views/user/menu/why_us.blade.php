cf735b83cef755055e83b10a5f53e907138fb922
Teguhriyadi <ilham.teguh55@gmail.com>
2022-08-26 01:10:54 +0700

    Merubah Menjadi Dinamis

--- resources/views/user/menu/why_us.blade.php
+++ resources/views/user/menu/why_us.blade.php
@@ -10,55 +10,39 @@
     </section>
     <section id="services" class="services section-bg">
         <div class="container" data-aos="fade-up">
-          <div class="section-title">
-            <h2>WHY CHOOSE US?</h2>
-            <p>
-              Magnam dolores commodi suscipit. Necessitatibus eius consequatur
-              ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam
-              quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea.
-              Quia fugiat sit in iste officiis commodi quidem hic quas.
-            </p>
-          </div>
-           <div class="row">
+            <div class="section-title">
+                <h2>WHY CHOOSE US?</h2>
+                <p>
+                    Magnam dolores commodi suscipit. Necessitatibus eius consequatur
+                    ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam
+                    quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea.
+                    Quia fugiat sit in iste officiis commodi quidem hic quas.
+                </p>
+            </div>
+            <div class="row">
+                @forelse ($data_why_us as $data)
                     <div class="col-sm-6 mb-4">
                         <div class="card detail">
-                        <div class="card-body">
-                            <h5 class="card-title">title treatment</h5>
-                            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                            <a href="" class="btn-sm btn-primary text-end">Read More</a>
-                        </div>
-                        </div>
-                    </div>
-                    <div class="col-sm-6 mb-4">
-                        <div class="card detail">
-                        <div class="card-body">
-                            <h5 class="card-title">Special title treatment</h5>
-                            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                            <a href="" class="btn-sm btn-primary text-end">Read More</a>
-                        </div>
+                            <div class="card-body">
+                                <h5 class="card-title">{{ $data->why_us_name }}</h5>
+                                <p class="card-text">{{ $data->why_us_deskripsi }}
+                                </p>
+                                <a href="{{ url('/why_us/' . $data->why_us_slug) }}" class="btn-sm btn-primary text-end">Read
+                                    Selengkapnya
+                                </a>
+                            </div>
                         </div>
                     </div>
+                @empty
+                    <div class="col-md-12">
+                        <i>
+                            <b>
+                                " Data Tidak Ada "
+                            </b>
+                        </i>
                     </div>
-                    <div class="row">
-                    <div class="col-sm-6 mb-4">
-                        <div class="card detail">
-                        <div class="card-body">
-                            <h5 class="card-title">Special title treatment</h5>
-                            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                            <a href="" class="btn-sm btn-primary text-end">Read More</a>
-                        </div>
-                        </div>
-                    </div>
-                    <div class="col-sm-6">
-                        <div class="card detail">
-                        <div class="card-body">
-                            <h5 class="card-title">Special title treatment</h5>
-                            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                            <a href="" class="btn-sm btn-primary text-end">Read More</a>
-                        </div>
-                        </div>
-                    </div>
-                    </div>
+                @endforelse
+            </div>
         </div>
-      </section>
+    </section>
 @endsection
