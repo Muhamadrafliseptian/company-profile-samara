59986033f2714f6bd0d980f0f61a83b2182def36
Muhamadrafliseptian <rafliseptiannn25@gmail.com>
2022-08-26 01:03:36 +0700

    about us

--- resources/views/user/menu/benefit.blade.php
+++ resources/views/user/menu/benefit.blade.php
@@ -1,29 +1,19 @@
-<section id="featured-services" class="featured-services">
-    <div class="container" data-aos="fade-up">
-        <div class="section-title">
-            <h2>Benefits</h2>
-            <h3><span>What is benefits can you got?</span></h3>
-        </div>
-        <div class="row">
+<div class="container">
+        <section id="box" class="icon-box">
+             <div class="row icon-boxes">
             @php
                 use App\Models\Pengaturan\Benefit;
                 $data_benefit = Benefit::get();
             @endphp
             @forelse ($data_benefit as $benefit)
-                <div class="col-md-6 col-lg-3 d-flex align-items-stretch mb-5">
-                    <div class="icon-box" data-aos="fade-up" data-aos-delay="100">
-                        <div class="icon">
-                            <i class="{{ $benefit->benefit_icon }}"></i>
-                        </div>
-                        <h4 class="title">
-                            <a href="">{{ $benefit->benefit_judul }}</a>
-                        </h4>
-                        <p class="description">
-                            {{ $benefit->benefit_deskripsi }}
-                        </p>
-                    </div>
-                </div>
-            @empty
+        <div class="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0" data-aos="zoom-in" data-aos-delay="200">
+            <div class="icon"><i class="{{ $benefit->benefit_icon }}"></i></div>
+          <div class="icon-box">
+            <h4 class="title"><a href="">{{ $benefit->benefit_judul }}</a></h4>
+            <p class="description">{{ $benefit->benefit_deskripsi }}</p>
+          </div>
+        </div>
+         @empty
                 <div class="col-md-12">
                     <div class="alert alert-danger text-center">
                         <i>
@@ -35,5 +25,5 @@
                 </div>
             @endforelse
         </div>
-    </div>
+      </div>
 </section>
